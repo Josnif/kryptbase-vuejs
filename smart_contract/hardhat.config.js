0befbe04855a9e6950d5e484c006d09a3717d9ef
joenif <josephnifemi89@gmail.com>
2022-03-21 15:28:25 +0100

    initial commit

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomiclabs/hardhat-waffle");
 
 const ALCHEMY_API_KEY = "1mqxw-OBwSSK9yV-fTK3eHULrFme0WHH";
 
-const ROPSTEN_PRIVATE_KEY = "8e9782556665488cc51c9f6378587abfeaf117645e5874269438aef3e70c9431";
+const ROPSTEN_PRIVATE_KEY = "";
 
 module.exports = {
   solidity: "0.8.0",
