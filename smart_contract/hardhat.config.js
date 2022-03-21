405028caa648710a0d98a6e88d7f5278f84a5dc0
joenif <josephnifemi89@gmail.com>
2022-03-21 21:27:59 +0100

    smart contract completed

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,9 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 const API_KEY = process.env.API_KEY;
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.0",
@@ -12,5 +14,8 @@ module.exports = {
       url: `https://eth-ropsten.alchemyapi.io/v2/${API_KEY}`,
       accounts: [`${PRIVATE_KEY}`]
     }
+  },
+  etherscan: {
+    apiKey: `${ETHERSCAN_API_KEY}`
   }
 };
